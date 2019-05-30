c7c9948c67fc37b5742ca522bce6fc0cbd245051
James.Sherman() <31998788+JamesonSherman@users.noreply.github.com>
2019-05-29 21:04:37 -0500

    wpf simple app that shows usage of stackpanel and listbox

---
+++ base_projects/wpf_stackpanel_listbox_visual_and_logical_tree/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace wpf_stackpanel_listbox_visual_and_logical_tree
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
