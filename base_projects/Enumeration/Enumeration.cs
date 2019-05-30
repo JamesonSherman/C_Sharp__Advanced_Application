3d7fef36a2415184a9966082fe30147cce80786c
James.Sherman() <31998788+JamesonSherman@users.noreply.github.com>
2019-05-29 21:01:03 -0500

    adds examples with C# Enumeration

---
+++ base_projects/Enumeration/Enumeration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumeration_examples
+{
+    /*
+    main advantage of enums is that they are basically integers with some named values, 
+    as such they are inherently portable and serializable.
+    Arithmetic and logical operations are also faster on enums.
+
+    Enumeration classes are used when you need an opaque value which has extra state information.
+    For instance a generic Data Access Layer could have as an interface like:
+    
+    public enum LightColors
+    {
+    unknown = 0,
+    red = 1,
+    yellow = 2,
+    green = 4,
+    green_arrow = 8
+    }
+
+    LightColors c = LightColors.red | LightColors.green_arrow;
+    
+    Enums are great for lightweight state information
+     */
+    enum Day { Mo, Tu, We, Th, Fr, Sa, Su};
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day fr = Day.Fr;
+            Day su = Day.Su;
+            Day a = Day.Fr;
+
+            System.Console.WriteLine(fr == a);
+        }
+    }
+}
