4a4b1f5aa348a16c7e1ab4bdbf71ef8e216c2304
James.Sherman() <31998788+JamesonSherman@users.noreply.github.com>
2019-10-16 03:31:42 -0500

    updated ds and algos with stack examples

---
+++ Data_Structures_and_Algorithms/Stacks/ArrayStack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Algorithms_DataStruct_Lib.Stacks
+{
+    public class ArrayStack<T> : IEnumerable<T>
+    {
+        private T[] _items;
+
+        public ArrayStack()
+        {
+            const int defaultCapacity = 4;
+            _items = new T[defaultCapacity];
+        }
+
+        public ArrayStack(int capacity)
+        {
+            _items = new T[capacity];
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException();
+            return _items[Count - 1];
+        }
+
+        public void Pop()
+        {
+            if(IsEmpty)
+                throw new InvalidOperationException();
+
+            _items[--Count] = default(T);
+        }
+
+        public void Push(T item)
+        {
+            if (_items.Length == Count)
+            {
+                T[] largerArray = new T[Count * 2];
+                Array.Copy(_items, largerArray, Count);
+
+                _items = largerArray;
+            }
+            _items[Count++] = item;            
+        }
+
+        public bool IsEmpty => Count == 0;
+        public int Count { get; private set; }
+        public int Capacity => _items.Length;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                yield return _items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Data_Structures_and_Algorithms/Stacks/LinkedStack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Algorithms_DataStruct_Lib.Stacks
+{
+    public class LinkedStack<T> : IEnumerable<T>
+    {
+        private readonly SinglyLinkedList<T> _list = new SinglyLinkedList<T>();
+
+        public T Peek()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException();
+            return _list.Head.Value;
+        }
+
+        public void Pop()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException();
+            _list.RemoveFirst();
+        }
+
+        public void Push(T item)
+        {
+            _list.AddFirst(item);
+        }
+
+        public bool IsEmpty => Count == 0;
+        public int Count => _list.Count;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
