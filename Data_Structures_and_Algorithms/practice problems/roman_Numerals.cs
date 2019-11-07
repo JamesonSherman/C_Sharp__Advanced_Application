ff70485d0b9fab0a2c00a33bb27b599aa7d9e83c
James.Sherman <31998788+JamesonSherman@users.noreply.github.com>
2019-11-07 03:51:58 -0600

    Update roman_Numerals.cs

--- Data_Structures_and_Algorithms/practice problems/roman_Numerals.cs
+++ Data_Structures_and_Algorithms/practice problems/roman_Numerals.cs
@@ -1,6 +1,6 @@
 public static string RomanNum(int number)
 {
-    if ((number < 0) || (number > 3999)) throw new ArgumentOutOfRangeException("insert value betwheen 1 and 3999");
+    if ((number < 0) || (number > 3999)) throw new ArgumentOutOfRangeException("insert value between 1 and 3999");
     if (number < 1) return string.Empty;            
     if (number >= 1000) return "M" + RomanNum(number - 1000);
     if (number >= 900) return "CM" + RomanNum(number - 900); 
